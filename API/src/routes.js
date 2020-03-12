45eb57867504e88e33e47a368d6202b8e505485a
Wesley-Oliveira <wesley0leby@gmail.com>
2020-03-12 17:26:45 -0300

    Adding hash with bcrypt

--- API/src/routes.js
+++ API/src/routes.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import Usercontroller from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/teste', async (req, res) => {
-  const user = await User.create({
-    name: 'Wesley',
-    email: 'wesley0leby@gmail.com',
-    password_hash: '123123',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', Usercontroller.store);
 
 export default routes;
